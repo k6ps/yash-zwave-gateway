ac24a9f65a37ff5e95fa21508112da47c519231d
k6ps <tanavst@gmail.com>
2017-02-13 11:23:46 +0200

    Added minimal implementatins of startup success and failure callbacks.

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ var zwave = new ZWave();
 var YashZwaveGateway = require('./yash-zwave-gateway.js');
 var yashZwaveGateway = new YashZwaveGateway(zwave);
 
-yashZwaveGateway.start();
+yashZwaveGateway.start(function() {
+    console.log("Z-Wave Gateway started successfuly.");
+}, function() {
+    console.log("Failed to start Z-Wave Gateway.");
+});
 
 process.on('SIGINT', function() {
     yashZwaveGateway.stop();
