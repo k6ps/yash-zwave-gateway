44290c28e9495712a3a4bc446cee0588ef06b976
k6ps <tanavst@gmail.com>
2017-03-11 10:22:43 +0200

    Fixed startup order

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 'use strict';
 
-var ZWave = require('openzwave-shared');
-var zwave = new ZWave();
-var YashZwaveGateway = require('./yash-zwave-gateway.js');
-var yashZwaveGateway = new YashZwaveGateway(zwave, yashTwitterMessenger);
 var Twitter = require('twitter');
+console.log('Twitter consumer key = '+process.env.TWITTER_CONSUMER_KEY);
 var twitter = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
     access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+
 var YashTwitterMessenger = require('./yash-twitter-messenger.js');
 var yashTwitterMessenger = new YashTwitterMessenger(twitter);
+
+var ZWave = require('openzwave-shared');
+var zwave = new ZWave();
+
+var YashZwaveGateway = require('./yash-zwave-gateway.js');
+var yashZwaveGateway = new YashZwaveGateway(zwave, yashTwitterMessenger);
 
 yashZwaveGateway.start();
 
