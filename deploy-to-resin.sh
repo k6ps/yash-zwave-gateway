ea5c56ab7a62ec0dd3336941a8dd555edaa517cc
k6ps <tanavst@gmail.com>
2018-02-15 21:58:02 +0200

    Attempt no 10 to auto-deploy from CircleCI to resin.io

--- deploy-to-resin.sh
+++ deploy-to-resin.sh
@@ -3,6 +3,7 @@
 DEPLOY_HOST=git.resin.io
 DEPLOY_USER=g_taavi_t_navsuu
 
+mkdir -p ~/.ssh
 ssh-keyscan -t ssh-rsa $DEPLOY_HOST >> ~/.ssh/known_hosts
 git remote add resin $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_USER/yashopenzwavegateway.git
 git push resin master
