0c08a7bfcc5fe5c138feb4995a33b994d8abb59c
Taavi Tänavsuu <tanavst@gmail.com>
2020-04-26 14:34:04 +0300

    Attempting installing form source, now in correct script..

--- yash/dependencies-amd64.sh
+++ yash/dependencies-amd64.sh
@@ -2,13 +2,18 @@
 
 # Required by OpenZWave
 echo "== Installing libudev-dev... =="
-sudo apt-get update && apt-get install -y apt-utils libudev-dev libudev0
+sudo apt-get update && \
+sudo apt-get install -y apt-utils libudev-dev libudev0
 echo "== Done installing libudev-dev. =="
 
 # install OpenZWave
 echo "== Installing OpenZWave ... =="
-wget http://mirror.my-ho.st/Downloads/OpenZWave/Debian_8.0/amd64/openzwave_1.4.164_amd64.deb
-wget http://mirror.my-ho.st/Downloads/OpenZWave/Debian_8.0/amd64/libopenzwave1.3-dev_1.4.164_amd64.deb
-wget http://mirror.my-ho.st/Downloads/OpenZWave/Debian_8.0/amd64/libopenzwave1.3_1.4.164_amd64.deb
-sudo dpkg -i *openzwave*.deb
+git clone https://github.com/OpenZWave/open-zwave.git && \
+cd ./open-zwave && \
+make && \
+make install
+# wget http://mirror.my-ho.st/Downloads/OpenZWave/Debian_8.0/amd64/openzwave_1.4.164_amd64.deb
+# wget http://mirror.my-ho.st/Downloads/OpenZWave/Debian_8.0/amd64/libopenzwave1.3-dev_1.4.164_amd64.deb
+# wget http://mirror.my-ho.st/Downloads/OpenZWave/Debian_8.0/amd64/libopenzwave1.3_1.4.164_amd64.deb
+# sudo dpkg -i *openzwave*.deb
 echo "== Done installing OpenZWave. =="
