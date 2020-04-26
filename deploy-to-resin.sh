3037c18089b4dc56f1191957d3431c7a424108e8
Taavi Tänavsuu <tanavst@gmail.com>
2020-04-26 18:55:36 +0300

    Fixed BalenaCloud deploy script.

--- deploy-to-resin.sh
+++ deploy-to-resin.sh
@@ -6,4 +6,4 @@ DEPLOY_USER=gh_k6ps
 mkdir -p ~/.ssh
 ssh-keyscan -t ssh-rsa $DEPLOY_HOST >> ~/.ssh/known_hosts
 git remote add balena $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_USER/yash.git
-git push resin master
+git push balena master
