0b970a79a5c2c78567ee75aed3a56d3088a9070e
Taavi Tänavsuu <tanavst@gmail.com>
2020-04-26 14:31:46 +0300

    Attempting to install OpenZWave from source.

--- yash/dependencies.sh
+++ yash/dependencies.sh
@@ -7,9 +7,13 @@ echo "== Done installing libudev-dev. =="
 
 # install OpenZWave 
 echo "== Installing OpenZWave ... =="
-wget https://github.com/ekarak/openzwave-debs-raspbian/raw/master/v1.4.79/openzwave_1.4.79.gfaea7dd_armhf.deb
-wget https://github.com/ekarak/openzwave-debs-raspbian/raw/master/v1.4.79/libopenzwave1.3_1.4.79.gfaea7dd_armhf.deb
-wget https://github.com/ekarak/openzwave-debs-raspbian/raw/master/v1.4.79/libopenzwave1.3-dev_1.4.79.gfaea7dd_armhf.deb
-sudo dpkg -i *openzwave*.deb
+git clone https://github.com/OpenZWave/open-zwave.git && \
+cd ./open-zwave && \
+make && \
+make install
+# wget https://github.com/ekarak/openzwave-debs-raspbian/raw/master/v1.4.79/openzwave_1.4.79.gfaea7dd_armhf.deb
+# wget https://github.com/ekarak/openzwave-debs-raspbian/raw/master/v1.4.79/libopenzwave1.3_1.4.79.gfaea7dd_armhf.deb
+# wget https://github.com/ekarak/openzwave-debs-raspbian/raw/master/v1.4.79/libopenzwave1.3-dev_1.4.79.gfaea7dd_armhf.deb
+# sudo dpkg -i *openzwave*.deb
 echo "== Done installing OpenZWave. =="
 
